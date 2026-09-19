640b4e8337523b56057b86663b97d84e677016e5
y0sh-dev <plside.best.sead@gmail.com>
2026-09-19 14:37:51 +0900

    wallpaper select.sh easier to use!!

--- .config/hypr/scripts/wallpaper_selector_pv.sh
+++ .config/hypr/scripts/wallpaper_selector_pv.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/.config/hypr/wallpaper"
+HIST="$HOME/.config/hypr/.wallpaper_history"
+touch "$HIST"
 
-find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | while read imageFile; do
-  echo -en "$(basename "$imageFile")\0icon\x1fthumbnail://$imageFile\n"
-done | rofi -dmenu -i -show-icons -config ~/.config/rofi/config.rasi >/tmp/selected_wallpaper_name.txt
+NEW_IMAGES=$(
+  find "$WALLPAPER_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -printf "%T@ %f\n" \
+    | sort -rn | cut -d' ' -f2- | grep -F -v -x -f "$HIST"
+)
 
-SELECTED_WALLPAPER_NAME=$(cat /tmp/selected_wallpaper_name.txt)
-rm /tmp/selected_wallpaper_name.txt
-
-if [ -z "$SELECTED_WALLPAPER_NAME" ]; then
-  exit 0
+if [ -n "$NEW_IMAGES" ]; then
+  (echo "$NEW_IMAGES"; cat "$HIST") > "$HIST.tmp" && mv "$HIST.tmp" "$HIST"
 fi
+
+SELECTED_WALLPAPER_NAME=$(
+  while read -r name; do
+    [[ -f "$WALLPAPER_DIR/$name" ]] && echo -en "$name\0icon\x1fthumbnail://$WALLPAPER_DIR/$name\n"
+  done < "$HIST" | rofi -dmenu -i -show-icons -config ~/.config/rofi/config.rasi
+)
+
+[[ -z "$SELECTED_WALLPAPER_NAME" ]] && exit 0
+
+(echo "$SELECTED_WALLPAPER_NAME"; grep -vxF "$SELECTED_WALLPAPER_NAME" "$HIST") > "$HIST.tmp" && mv "$HIST.tmp" "$HIST"
 
 FULL_PATH="$WALLPAPER_DIR/$SELECTED_WALLPAPER_NAME"
 
 awww img "$FULL_PATH" --transition-type center --transition-step 199 --transition-duration 2.9
-
 wpg -s "$FULL_PATH"
-
-echo "$FULL_PATH" >~/.config/hypr/current_wallpaper_awww
+echo "$FULL_PATH" > ~/.config/hypr/current_wallpaper_awww
 
 sleep 1s
 hyprctl reload
